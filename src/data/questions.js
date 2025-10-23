8792b333e6b498b5980e05aaba1c1c1ba9e96f44
Iñaki Gisbert <igisbertmartinez@gmail.com>
2025-10-23 19:44:35 +0200

    Agregar fuente Poppins, CSS y archivo de preguntas

---
+++ src/data/questions.js
@@ -0,0 +1,381 @@
+const machipalabra = [
+  {
+    letra: "A",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por A: Apellido del jugador con el que Juanma se compara al subirse las gafas y verse la cara",
+        respuesta: "Ayton",
+      },
+      {
+        pregunta:
+          "Empieza por A. Animal que vive en el agua y respira por branquias",
+        respuesta: "atún",
+      },
+    ],
+  },
+  {
+    letra: "B",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por B. Deporte que se juega con un balón naranja y una canasta",
+        respuesta: "baloncesto",
+      },
+      {
+        pregunta:
+          "Contiene la B. Instrumento musical de cuerda que se toca rasgando",
+        respuesta: "guitarra",
+      },
+    ],
+  },
+  {
+    letra: "C",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por C. Nombre de la marca de cerveza que Pepe confiesa confundir con la Estrella Galicia",
+        respuesta: "Cruzcampo",
+      },
+      {
+        pregunta:
+          "Empieza por C. Lo que Pepe y Juanma no saben ponerse y por lo van con ella en la mano, esperando que alguien decente les ayude.",
+        respuesta: "Corbata",
+      },
+    ],
+  },
+  {
+    letra: "D",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por D. Sistema político que, según Pepe, no funciona.",
+        respuesta: "Democracia",
+      },
+      {
+        pregunta:
+          "Empieza por D. Ciudad de la costa donde, según Toni, ir mamao por la calle a las seis de la tarde está mal visto.",
+        respuesta: "Dénia",
+      },
+    ],
+  },
+  {
+    letra: "E",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por E. Jugador que, según Toni, nunca se ve con un 'cuerpo griego', sino que siempre está con 'tres hamburguesas de más'.",
+        respuesta: "Embiid",
+      },
+      {
+        pregunta:
+          "Contiene la E. Conjunto de siete colores que aparece tras la lluvia",
+        respuesta: "arcoíris",
+      },
+    ],
+  },
+  {
+    letra: "F",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por F. Instrumento musical que, según Pepe, al ser enseñado a los niños en el colegio consigue crear gente que detesta la música para el resto de su vida.",
+        respuesta: "Flauta",
+      },
+      {
+        pregunta:
+          "Empieza la F. Toni sugiere que, si no es para esto, bailar no tiene mucho sentido.",
+        respuesta: "Follar",
+      },
+    ],
+  },
+  {
+    letra: "G",
+    preguntas: [
+      {
+        pregunta: "Empieza por G. Ave de corral más grande que la gallina",
+        respuesta: "gallo",
+      },
+      {
+        pregunta: "Contiene la G. Instrumento musical de cuerda pulsada",
+        respuesta: "guitarra",
+      },
+    ],
+  },
+  {
+    letra: "H",
+    preguntas: [
+      {
+        pregunta: "Empieza por H. Alimento que elaboran las abejas",
+        respuesta: "miel",
+      },
+      {
+        pregunta: "Empieza por H. Lugar donde se atiende a los enfermos",
+        respuesta: "hospital",
+      },
+    ],
+  },
+  {
+    letra: "I",
+    preguntas: [
+      {
+        pregunta: "Empieza por I. Continente helado del Polo Norte",
+        respuesta: "ártico",
+      },
+      {
+        pregunta: "Empieza por I. País con forma de bota en Europa",
+        respuesta: "Italia",
+      },
+    ],
+  },
+  {
+    letra: "J",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por J. Figura histórica que Juanma describe como 'uno de los primeros casos, en teoría documentados si es verdad la Biblia, de un zombi'",
+        respuesta: "Jesucristo,Jesús",
+      },
+      {
+        pregunta: "Contiene la J. Animal felino manchado que vive en la selva",
+        respuesta: "jaguar",
+      },
+    ],
+  },
+  {
+    letra: "K",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por K. Nombre y apellido del jugador cuyas declaraciones sobre su exequipo a Toni le suenan a novia despechada.",
+        respuesta: "Klay Thompson",
+      },
+      {
+        pregunta: "Contiene la K. Deporte japonés de defensa personal",
+        respuesta: "karate",
+      },
+    ],
+  },
+  {
+    letra: "L",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por L. Objeto que sirve para alumbrar en la oscuridad",
+        respuesta: "linterna",
+      },
+      {
+        pregunta: "Contiene la L. Astro que ilumina la Tierra de noche",
+        respuesta: "luna",
+      },
+    ],
+  },
+  {
+    letra: "M",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por M. Apellido del jugador al que Juanma afirma que no le dieron un iPhone por idiota y que todo lo que le pasa suele ser por idiota",
+        respuesta: "Morris",
+      },
+      {
+        pregunta:
+          "Empieza por M. Nombre que reciben Vucevic, DeRozan y Lavine al juntarse en los Bulls",
+        respuesta: "Mid Three",
+      },
+    ],
+  },
+  {
+    letra: "N",
+    preguntas: [
+      {
+        pregunta: "Empieza por N. Color que es la ausencia de luz",
+        respuesta: "negro",
+      },
+      {
+        pregunta: "Contiene la N. Mes del año con 28 o 29 días",
+        respuesta: "febrero",
+      },
+    ],
+  },
+  {
+    letra: "Ñ",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por Ñ. Animal grande y peludo que habita en Sudamérica",
+        respuesta: "ñu",
+      },
+      {
+        pregunta: "Contiene la Ñ. Persona de poca edad",
+        respuesta: "niño",
+      },
+    ],
+  },
+  {
+    letra: "O",
+    preguntas: [
+      {
+        pregunta: "Empieza por O. Animal acuático con ocho tentáculos",
+        respuesta: "pulpo",
+      },
+      {
+        pregunta: "Empieza por O. Metal precioso de color amarillo",
+        respuesta: "oro",
+      },
+    ],
+  },
+  {
+    letra: "P",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por P. Objetos que Juanma odia profundamente, ya que le parece un concepto estúpido que no entiende cómo puede ser divertido",
+        respuesta: "Petardos",
+      },
+      {
+        pregunta: "Contiene la P. Objeto que usamos para escribir",
+        respuesta: "lápiz",
+      },
+    ],
+  },
+  {
+    letra: "Q",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por Q. Marca de tienda de campaña que Pepe perdió junto a 200 pesetas y una novia en un festival de Benicàssim en el 96",
+        respuesta: "Quechua",
+      },
+      {
+        pregunta: "Contiene la Q. Prenda de vestir que cubre el tronco",
+        respuesta: "chaqueta",
+      },
+    ],
+  },
+  {
+    letra: "R",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por R. Cantante a la que Juanma menciona al decir que ha estado un poco 'de lao a lao'",
+        respuesta: "Rosalía",
+      },
+      {
+        pregunta: "Empieza por R. Color del cielo en un día despejado",
+        respuesta: "azul",
+      },
+    ],
+  },
+  {
+    letra: "S",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por S. Apellido del entrenador que Toni menciona que le gustaría presentarle a su hermana, porque de ser como en el baloncesto, sería una mujer muy feliz",
+        respuesta: "Spoelstra",
+      },
+      {
+        pregunta: "Contiene la S. Vehículo que circula sobre raíles",
+        respuesta: "tren",
+      },
+    ],
+  },
+  {
+    letra: "T",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por T. Red social de citas que funciona como una tragaperras del amor",
+        respuesta: "Tinder",
+      },
+      {
+        pregunta: "Contiene la T. Aparato para ver programas y series",
+        respuesta: "televisión",
+      },
+    ],
+  },
+  {
+    letra: "U",
+    preguntas: [
+      {
+        pregunta: "Empieza por U. Fruto morado que crece en racimos",
+        respuesta: "uva",
+      },
+      {
+        pregunta: "Contiene la U. Edificio donde se imparte educación superior",
+        respuesta: "universidad",
+      },
+    ],
+  },
+  {
+    letra: "V",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por V. Guerra de la que Machicado tiene la mirada de los veteranos, según Pepe, tras pasar dos años en el grupo Prisa",
+        respuesta: "Vietnam",
+      },
+      {
+        pregunta: "Empieza por V. Estación del año entre primavera y otoño",
+        respuesta: "verano",
+      },
+    ],
+  },
+  {
+    letra: "W",
+    preguntas: [
+      {
+        pregunta:
+          "Contiene la W. Apellido del portero del Borussia Dortmund al que Machicado le quitó un taxi para vengarse por haberle arrebatado una Champions.",
+        respuesta: "Weidenfeller",
+      },
+      {
+        pregunta:
+          "Empieza por W. Bebida alcohólica destilada de origen escocés",
+        respuesta: "whisky",
+      },
+    ],
+  },
+  {
+    letra: "X",
+    preguntas: [
+      {
+        pregunta: "Contiene la X. Instrumento musical de láminas de metal",
+        respuesta: "xilófono",
+      },
+      {
+        pregunta: "Contiene la X. Continuo de números que suman diez",
+        respuesta: "seis",
+      },
+    ],
+  },
+  {
+    letra: "Y",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por Y. Calificativo usado por Pepe para describir la chaqueta que lleva Machicado al evento que hicieron con público",
+        respuesta: "Yonki",
+      },
+      {
+        pregunta: "Contiene la Y. Vestido de novia de color blanco",
+        respuesta: "traje",
+      },
+    ],
+  },
+  {
+    letra: "Z",
+    preguntas: [
+      {
+        pregunta:
+          "Empieza por Z. Jugador cuyo apodo creado por Machicado impide que le den premios al podcast.",
+        respuesta: "Zubac",
+      },
+      {
+        pregunta: "Contiene la Z. Color de la nieve",
+        respuesta: "blanco",
+      },
+    ],
+  },
+];
