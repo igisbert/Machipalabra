7cc7d626673ad1489a6f0c5ceb70790369b8104d
Iñaki Gisbert <igisbertmartinez@gmail.com>
2025-10-24 14:26:12 +0200

    Agregar configuración del servidor en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,8 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
+  server: {
+    host: true, // O puedes usar "0.0.0.0" en lugar de true
+    port: 4321, // Si quieres un puerto específico
+  },
 });
